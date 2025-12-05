e7af518c965a1f1561f251efe20e177825281172
Charlotte Norment <charlottenorment@Charlottes-MacBook-Air-902.local>
2025-12-05 11:13:20 -0500

    worked on def main()

--- Data-Collection/listener_data.py
+++ Data-Collection/listener_data.py
@@ -4,3 +4,5 @@ import requests
 import json
 
 def listener_data(api_key): 
+
+API_KEY = "	e7daacfe434e52e4cfd7c8f6b1004d29"
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-def main(): 
+# Charlotte 
+
+def main():
+    print("Starting Ticket Trend Tracking...\n")
+
+    # Load API keys
+    lastfm_key = input()
+    ticketmaster_key = input()
+
+    # Gather data from APIs
+    print("Gathering listener data from Last.fm...")
+    listener_dict = listener_data(lastfm_key)
+
+    print("Gathering ticket data from Ticketmaster...")
+    ticket_dict = ticket_data(ticketmaster_key) 
+
+    # Create database / store data
+    print("Creating database...")
+    db_name = "ticket_trends.sqlite"
+    create_database(db_name)
+
+    # Run calculations 
+    print("Running calculations...")
+    calculation_results = calculations(db_name)
+
+    # Save results to text file 
+    print("Saving calculation results...")
+    final_calculation_results(calculation_results)
+
+    # Create visualizations 
+    print("Creating bar chart...")
+    create_bar_chart(calculation_results)
+
+    print("Creating scatter plot...")
+    create_scatter_plot(calculation_results)
+
+    print("\nTicket Trend Tracking Complete!")
