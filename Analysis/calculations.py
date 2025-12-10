1c0e9f3b814b91a68c0fd0ad91566e7fbc15a8d8
Emerson McKay <emckayy@umich.edu>
2025-12-10 12:31:29 -0500

    Fixed calculations

--- Analysis/calculations.py
+++ Analysis/calculations.py
@@ -8,18 +8,18 @@ def calculations(db_name):
     cur = conn.cursor()
 
     cur.execute("""
-            SELECT
-                artists.artist_name,
-                artists.listeners,
-                COUNT(events.event_id) AS num_concerts,
-                COUNT(DISTINCT venues.venue_id) AS num_venues
-            FROM events
-            LEFT JOIN events ON artists.artist_name = events.artist_name
-            LEFT JOIN venues ON events.venue_id = venues.venue_id
-            GROUP BY events.artist_name
-            HAVING num_concerts > 0
-            ORDER BY num_concerts DESC;       
-            """)
+        SELECT 
+            artists.artist_name,
+            artists.listeners,
+            COUNT(events.event_id) AS num_concerts,
+            COUNT(DISTINCT venues.venue_id) AS num_venues
+        FROM artists
+        LEFT JOIN events ON artists.artist_name = events.artist_name
+        LEFT JOIN venues ON events.venue_id = venues.venue_id
+        GROUP BY artists.artist_name
+        HAVING num_concerts > 0
+        ORDER BY num_concerts DESC;
+    """)
 
     results = cur.fetchall()
     conn.close()
@@ -28,6 +28,6 @@ def calculations(db_name):
 
 if __name__ == "__main__":
     db_name = "ticket_trends.sqlite"  
-    print("Artist, num of concerts, and number of unique venues:")
+    print("Artist, num of listeners, num of concerts, and number of unique venues:")
     print(calculations("ticket_trends.sqlite"))
 
