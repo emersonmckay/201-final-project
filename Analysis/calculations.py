6f2d533bb6e6a9cef31055b9f9f3a8c55dbb38a3
Emerson McKay <emckayy@umich.edu>
2025-12-10 11:36:08 -0500

    Updated the calculations file to be num of events per artist instead of venue capacity since Ticketmaster doesn't provide that data

--- Analysis/calculations.py
+++ Analysis/calculations.py
@@ -7,14 +7,18 @@ def calculations(db_name):
     conn = sqlite3.connect(db_name)
     cur = conn.cursor()
 
-    # artist name, number of concerts, number of unique venues
+    # artist name, number of concerts, number of events per artists (artists.___ will be
+    # will be null until we have last.fm data
     cur.execute("""
             SELECT
+                artists.artist_name,
+                artists.listeners,
                 events.artist_name,
                 COUNT(events.event_id) AS num_concerts,
                 COUNT(DISTINCT venues.venue_id) AS num_venues
             FROM events
-            JOIN venues ON events.venue_id = venues.venue_id
+            LEFT JOIN events ON artists.artist_name = events.artist_name
+            LEFT JOIN venues ON events.venue_id = venues.venue_id
             GROUP BY events.artist_name
             ORDER BY num_concerts DESC;       
             """)
